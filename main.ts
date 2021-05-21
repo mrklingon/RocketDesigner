c8ed42fdceb45e04a66c72e40856ee6c548ee88a
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-05-20 21:22:59 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,36 @@
+input.onButtonPressed(Button.A, function () {
+    if ("Ballistic" == Modes[Mode]) {
+        power += 1
+        if (power > 3) {
+            power = 0
+        }
+        basic.showString("P = " + convertToText(power))
+    }
+})
+function doBall () {
+    rocket = game.createSprite(0, 3)
+    rocket.turn(Direction.Left, 45)
+    for (let index = 0; index < power * 3; index++) {
+        rocket.move(0.5)
+        basic.pause(200)
+    }
+    basic.pause(500)
+    rocket.delete()
+}
 input.onButtonPressed(Button.AB, function () {
     Mode += 1
-    if (Mode > 2) {
+    if (Mode > 3) {
         Mode = 0
     }
     basic.showString("" + (Modes[Mode]))
+})
+input.onButtonPressed(Button.B, function () {
+    if ("Launch" == Modes[Mode]) {
+        DoLaunch()
+    }
+    if ("Ballistic" == Modes[Mode]) {
+        doBall()
+    }
 })
 function DoLaunch () {
     basic.showLeds(`
@@ -38,10 +65,12 @@ function DoLaunch () {
         . . . . .
         `)
 }
+let rocket: game.LedSprite = null
+let power = 0
 let Modes: string[] = []
 let Mode = 0
 Mode = 0
-Modes = ["Launch", "Design", "Test"]
+Modes = ["Launch", "Design", "Test", "Ballistic"]
 basic.showLeds(`
     . . . . .
     . . # . .
@@ -73,6 +102,4 @@ basic.showLeds(`
     . . . . .
     . . . . .
     `)
-basic.forever(function () {
-	
-})
+power = 0
