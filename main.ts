d363f29ce961f01705b2073bb3d5036b5fb50f57
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-05-21 15:14:01 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,7 +6,21 @@ input.onButtonPressed(Button.A, function () {
         }
         basic.showString(convertToText(power))
     }
+    if ("D" == Modes[Mode]) {
+        Dmode += 1
+        if (Dmode > 1) {
+            Dmode = 0
+        }
+        basic.showString("" + (Dmodes[Dmode]))
+    }
+    if ("T" == Modes[Mode]) {
+        basic.showString("FPS")
+        basic.showString("" + (doVel()))
+    }
 })
+function doVel () {
+    return Time * ((Force / Mass - 1) * 32.2)
+}
 function doBall () {
     Stp = 0.5
     rocket = game.createSprite(0, 3)
@@ -33,6 +47,22 @@ input.onButtonPressed(Button.B, function () {
     }
     if ("B" == Modes[Mode]) {
         doBall()
+    }
+    if ("D" == Modes[Mode]) {
+        if ("F" == Dmodes[Dmode]) {
+            Force += 1
+            if (Force > 9) {
+                Force = 2
+            }
+            basic.showString("" + (Force))
+        }
+        if ("T" == Dmodes[Dmode]) {
+            Time += 1
+            if (Time > 9) {
+                Time = 2
+            }
+            basic.showString("" + (Time))
+        }
     }
 })
 function DoLaunch () {
@@ -71,39 +101,19 @@ function DoLaunch () {
 let rocket: game.LedSprite = null
 let Stp = 0
 let power = 0
+let Mass = 0
+let Time = 0
+let Force = 0
+let Dmodes: string[] = []
 let Modes: string[] = []
 let Mode = 0
+let Dmode = 0
+Dmode = 0
 Mode = 0
 Modes = ["L", "D", "T", "B"]
-basic.showLeds(`
-    . . . . .
-    . . # . .
-    . . # . .
-    . # # # .
-    . # # # .
-    `)
-basic.pause(600)
-basic.showLeds(`
-    . . # . .
-    . . # . .
-    . # # # .
-    . # # # .
-    . . . . .
-    `)
-basic.pause(200)
-basic.showLeds(`
-    . . # . .
-    . . # . .
-    . # # # .
-    . . . . .
-    . . . . .
-    `)
-basic.pause(100)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
+Dmodes = ["F", "T"]
+Force = 2
+Time = 2
+Mass = 1
 power = 0
+DoLaunch()
